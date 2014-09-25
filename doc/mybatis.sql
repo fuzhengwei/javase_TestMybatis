ad1765c78220bd6672dbcc25681e98aeafa6384d
fuzhengwei <184172133@qq.com>
2014-09-25 20:23:03 +0800

    测试多表查询

--- doc/mybatis.sql
+++ doc/mybatis.sql
@@ -1,10 +1,10 @@
 /*
 MySQL Data Transfer
-Source Host: 127.0.0.1
+Source Host: localhost
 Source Database: mybatis
-Target Host: 127.0.0.1
+Target Host: localhost
 Target Database: mybatis
-Date: 2014/9/24 16:15:09
+Date: 2014-9-25 20:22:13
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -15,7 +15,7 @@ CREATE TABLE `department` (
   `deparment_id` int(11) NOT NULL AUTO_INCREMENT,
   `deparment_name` varchar(20) NOT NULL COMMENT '部门名称',
   PRIMARY KEY (`deparment_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for emp
@@ -26,8 +26,13 @@ CREATE TABLE `emp` (
   `emp_name` varchar(20) NOT NULL,
   `emp_pwd` varchar(20) NOT NULL,
   PRIMARY KEY (`emp_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records 
 -- ----------------------------
+INSERT INTO `department` VALUES ('1', '物流与专销平台');
+INSERT INTO `department` VALUES ('2', '政府事业部');
+INSERT INTO `emp` VALUES ('1', '1', 'admin', '123');
+INSERT INTO `emp` VALUES ('2', '1', 'root', '123');
+INSERT INTO `emp` VALUES ('3', '2', 'sa', 'sa');
